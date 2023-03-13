e4386d7d9b2c8705b263cb631a5c50ba116fc747
Galacsh <galacsh.dev@gmail.com>
2023-03-13 18:46:37 +0900

    feat: reading view keyboard fold/unfold feature

--- README.md
+++ README.md
@@ -1,96 +1,25 @@
-# Obsidian Sample Plugin
+# Reading View Enhancer plugin for Obsidian
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+This is a plugin for [Obsidian](https://obsidian.md/) that enhances the reading experience in preview mode
+by adding a "collapse indicator" selector. Use arrow keys to select next/previous "collapse indicator" and
+fold/unfold easily.
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+## Features
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+- `Reading View Enhancer: Activate selector`
+  - Start using the "collapse indicator" selector
+- `Reading View Enhancer: Deactivate selector`
+  - Turn back to default reading view by deactivating the selector
+  - By default, `Esc` deactivates the selector
+- Use arrow keys to interact with "collapse indicator"s
+  - `Arrow Up`: Select previous "collapse indicator"
+  - `Arrow Down`: Select next "collapse indicator"
+- Click to select nearest "collapse indicator" from clicked position
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Changes the default font color to red using `styles.css`.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+## Installation
 
-## First time developing plugins?
-
-Quick starting guide for new plugin devs:
-
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
-
-## Releasing new releases
-
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
-
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
-
-## Adding your plugin to the community plugin list
-
-- Check https://github.com/obsidianmd/obsidian-releases/blob/master/plugin-review.md
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
-
-## How to use
-
-- Clone this repo.
-- `npm i` or `yarn` to install dependencies
-- `npm run dev` to start compilation in watch mode.
-
-## Manually installing the plugin
-
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
-
-## Funding URL
-
-You can include funding URLs where people who use your plugin can financially support it.
-
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
-```
-
-If you have multiple URLs, you can also do:
-
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
-```
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+- Download the latest release from the [releases page](https://github.com/Galacsh/obsidian-reading-view-enhancer/releases)
+- Extract the downloaded folder into your Obsidian plugins folder
+- Restart Obsidian if it is running
+  - Or, refresh the `Community plugins > Installed plugins`
+- Enable the plugin in the `Installed plugins`
--- main.ts
+++ main.ts
@@ -1,137 +1,273 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { MarkdownView, Plugin } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class ReadingViewEnhancer extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		const { workspace } = this.app;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// Loosing cursor effect
+		this.registerEvent(workspace.on("editor-change", this.deactivate));
+		this.registerEvent(workspace.on("active-leaf-change", this.deactivate));
+		this.registerEvent(workspace.on("layout-change", this.deactivate));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// Command for activating selector
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
+			id: "reading-mode-activate-selector",
+			name: "Activate selector",
+			checkCallback: (checking) => {
+				if (this.isPreviewMode()) {
+					if (!checking) this.activate();
 					return true;
-				}
+				} else return false;
+			},
+		});
+
+		// Command for deactivating selector
+		this.addCommand({
+			id: "reading-mode-deactivate-selector",
+			name: "Deactivate selector",
+			checkCallback: (checking) => {
+				if (this.isPreviewMode()) {
+					if (!checking) this.deactivate();
+					return true;
+				} else return false;
+			},
+		});
+	}
+
+	/**
+	 * Returns true if active view is in preview(reading) mode
+	 */
+	isPreviewMode(): boolean {
+		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		return activeView?.getMode() === "preview";
+	}
+
+	/**
+	 * Activate collapse manager
+	 */
+	activate(): void {
+		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		const previewContainer = activeView?.previewMode.containerEl;
+
+		if (previewContainer) {
+			const collapseManager = CollapseIndicatorManager.getInstance();
+			collapseManager.activate(previewContainer);
+		} else {
+			console.log("No preview container to activate Reading Mode Cursor.");
+		}
+	}
+
+	/**
+	 * Deactivate collapse manager
+	 */
+	deactivate(): void {
+		const blockSelector = CollapseIndicatorManager.getInstance();
+		blockSelector.deactivate();
+	}
+}
+
+class CollapseIndicatorManager {
+	private static instance: CollapseIndicatorManager;
+	private isActive = false;
+	private rootElement: HTMLElement | null;
+	private selectedIndex: number;
+	private selector: HTMLElement | null;
+	private indicators: HTMLElement[];
+	private observer: MutationObserver;
+	private arrowKeyListener: (ev: KeyboardEvent) => void;
+	private clickListener: (ev: MouseEvent) => void;
+
+	private constructor() {
+		this.isActive = false;
+		this.rootElement = null;
+		this.selectedIndex = -1;
+		this.selector = null;
+		this.indicators = [];
+		this.observer = new MutationObserver(() => {
+			this.updateIndicators();
+		});
+		this.arrowKeyListener = this.handleArrowEvent.bind(this);
+		this.clickListener = this.handleClickEvent.bind(this);
+	}
+
+	public static getInstance(): CollapseIndicatorManager {
+		if (!CollapseIndicatorManager.instance) {
+			CollapseIndicatorManager.instance = new CollapseIndicatorManager();
+		}
+
+		return CollapseIndicatorManager.instance;
+	}
+
+	public activate(rootElement: HTMLElement): void {
+		if (this.isActive) return;
+
+		this.isActive = true;
+		this.rootElement = rootElement;
+
+		this.updateIndicators();
+		this.selectNext();
+
+		this.observer.observe(rootElement, {
+			childList: true,
+			subtree: true,
+			attributes: true,
+		});
+
+		this.addEventListeners();
+	}
+
+	public deactivate(): void {
+		if (!this.isActive) return;
+
+		this.isActive = false;
+
+		this.removeEventListeners();
+		this.observer?.disconnect();
+
+		// Remove selector
+		this.selector?.classList.remove("rmc-selected");
+		this.selectedIndex = -1;
+		this.selector = null;
+
+		this.rootElement = null;
+	}
+
+	private addEventListeners(): void {
+		document.body.addEventListener("keydown", this.arrowKeyListener);
+		this.rootElement?.addEventListener("click", this.clickListener);
+	}
+
+	private removeEventListeners(): void {
+		document.body.removeEventListener("keydown", this.arrowKeyListener);
+		this.rootElement?.removeEventListener("click", this.clickListener);
+	}
+
+	private handleArrowEvent(event: KeyboardEvent) {
+		if (this.isActive) {
+			switch (event.key) {
+				case "ArrowUp":
+					event.preventDefault();
+					this.selectPrevious();
+					this.scrollToSelector();
+					break;
+				case "ArrowDown":
+					event.preventDefault();
+					this.selectNext();
+					this.scrollToSelector();
+					break;
+				case "ArrowLeft":
+				case "ArrowRight":
+					event.preventDefault();
+					this.selector?.click(); // 실제 클릭 이벤트 단 거랑 같이 동작돼서 selected 날아감 : TODO
+					break;
+				case "Escape":
+					this.deactivate();
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
+	private handleClickEvent(event: MouseEvent): void {
+		const clicked = event.target as HTMLElement;
+		if (clicked.classList?.contains("collapse-indicator")) return;
+		else if (
+			clicked.tagName === "a" ||
+			clicked.tagName === "input" ||
+			clicked.tagName === "button"
+		) {
+			return;
+		} else event.stopPropagation();
+
+		const nearestIndex = this.findNearestUpperIndicatorIndex(
+			event.clientX,
+			event.clientY
+		);
+		this.select(nearestIndex);
+	}
+
+	private findNearestUpperIndicatorIndex(x: number, y: number): number {
+		let nearestIndex = -1;
+		let nearestDistance = Number.MAX_VALUE;
+
+		this.indicators.forEach((indicator, index) => {
+			const distance = this.getDistanceToUpperElement({ x, y }, indicator);
+
+			if (distance < nearestDistance) {
+				nearestDistance = distance;
+				nearestIndex = index;
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		return nearestIndex;
+	}
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+	private getDistanceToUpperElement(
+		{ x, y }: { x: number; y: number },
+		upperElement: HTMLElement
+	): number {
+		const upper = upperElement.getBoundingClientRect();
+
+		const dy = y - upper.top;
+
+		if (dy < 0) return Number.MAX_VALUE;
+		else return dy;
+	}
+
+	private select(index: number): void {
+		this.selector?.classList.remove("rmc-selected");
+
+		this.selectedIndex = index;
+		if (index === -1) this.selector = null;
+		else this.selector = this.indicators[index];
+
+		this.selector?.classList.add("rmc-selected");
+	}
+
+	private selectPrevious(): void {
+		if (this.indicators.length === 0 || this.selectedIndex <= 0) return;
+		this.select(this.selectedIndex - 1);
+	}
+
+	private selectNext(): void {
+		if (
+			this.indicators.length === 0 ||
+			this.selectedIndex >= this.indicators.length - 1
+		) {
+			return;
+		}
+
+		this.select(this.selectedIndex + 1);
+	}
+
+	private scrollToSelector(): void {
+		this.selector?.scrollIntoView({
+			behavior: "smooth",
+			block: "center",
+			inline: "center",
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
+	private updateIndicators(): void {
+		this.indicators = this.findSelectable();
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	private findSelectable(): HTMLElement[] {
+		if (this.rootElement == null) return [];
+
+		return Array.from(
+			this.rootElement.querySelectorAll(".collapse-indicator")
+		).filter(this.isSelectable) as HTMLElement[];
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+	private isSelectable(indicator: HTMLElement): boolean {
+		const parent = indicator.parentElement;
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		if (parent?.closest(".collapse-indicator")?.closest(".is-collapsed"))
+			return false;
+		else if (parent?.closest("ol")?.closest(".is-collapsed")) return false;
+		else if (parent?.closest("ul")?.closest(".is-collapsed")) return false;
+		else return true;
 	}
 }
